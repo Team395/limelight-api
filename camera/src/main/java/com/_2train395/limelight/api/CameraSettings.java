5c7e0014f2d4467098a958e81be4185b2ccf9f2e
Cole French <16979554+ColeFrench@users.noreply.github.com>
2019-01-11 16:04:30 -0500

    Convert CameraSettings class from a utility to an object

--- camera/src/main/java/com/_2train395/limelight/api/CameraSettings.java
+++ camera/src/main/java/com/_2train395/limelight/api/CameraSettings.java
@@ -1,43 +1,85 @@
 package com._2train395.limelight.api;
 
 public class CameraSettings {
-    public static LedMode getLedMode() {
-        return LedMode.values()[Limelight.getTable().getEntry(LedMode.KEY).getNumber(0).intValue()];
+    private LedMode ledMode;
+    private CameraMode cameraMode;
+    private Pipeline pipeline;
+    private StreamMode streamMode;
+    private SnapshotMode snapshotMode;
+
+    CameraSettings() {
+        ledMode = fetchLedMode();
+        cameraMode = fetchCameraMode();
+        pipeline = fetchPipeline();
+        streamMode = fetchStreamMode();
+        snapshotMode = fetchSnapshotMode();
     }
 
-    public static void setLedMode(final LedMode ledMode) {
-        Limelight.getTable().getEntry(LedMode.KEY).setNumber(ledMode.ordinal());
+    public CameraSettings(final LedMode ledMode, final CameraMode cameraMode, final Pipeline pipeline, final StreamMode streamMode, final SnapshotMode snapshotMode) {
+        this.ledMode = ledMode;
+        this.cameraMode = cameraMode;
+        this.pipeline = pipeline;
+        this.streamMode = streamMode;
+        this.snapshotMode = snapshotMode;
     }
 
-    public static CameraMode getCameraMode() {
-        return CameraMode.values()[Limelight.getTable().getEntry(CameraMode.KEY).getNumber(0).intValue()];
+    private static LedMode fetchLedMode() {
+        return LedMode.values()[(int) Limelight.getTable().getEntry(LedMode.KEY).getDouble(0.0)];
     }
 
-    public static void setCameraMode(final CameraMode cameraMode) {
-        Limelight.getTable().getEntry(CameraMode.KEY).setNumber(cameraMode.ordinal());
+    private static CameraMode fetchCameraMode() {
+        return CameraMode.values()[(int) Limelight.getTable().getEntry(CameraMode.KEY).getDouble(0.0)];
     }
 
-    public static Pipeline getCurrentPipeline() {
-        return Pipeline.values()[Limelight.getTable().getEntry(Pipeline.KEY).getNumber(0).intValue()];
+    private static Pipeline fetchPipeline() {
+        return Pipeline.values()[(int) Limelight.getTable().getEntry(Pipeline.KEY).getDouble(0.0)];
     }
 
-    public static void setCurrentPipeline(final Pipeline pipeline) {
-        Limelight.getTable().getEntry(Pipeline.KEY).setNumber(pipeline.ordinal());
+    private static StreamMode fetchStreamMode() {
+        return StreamMode.values()[(int) Limelight.getTable().getEntry(StreamMode.KEY).getDouble(0.0)];
     }
 
-    public static StreamMode getStreamMode() {
-        return StreamMode.values()[Limelight.getTable().getEntry(StreamMode.KEY).getNumber(0).intValue()];
+    private static SnapshotMode fetchSnapshotMode() {
+        return SnapshotMode.values()[(int) Limelight.getTable().getEntry(SnapshotMode.KEY).getDouble(0.0)];
     }
 
-    public static void setStreamMode(final StreamMode streamMode) {
-        Limelight.getTable().getEntry(StreamMode.KEY).setNumber(streamMode.ordinal());
+    public LedMode getLedMode() {
+        return ledMode;
     }
 
-    public static SnapshotMode getSnapshotMode() {
-        return SnapshotMode.values()[Limelight.getTable().getEntry(SnapshotMode.KEY).getNumber(0).intValue()];
+    public void setLedMode(final LedMode ledMode) {
+        this.ledMode = ledMode;
     }
 
-    public static void setSnapshotMode(final SnapshotMode snapshotMode) {
-        Limelight.getTable().getEntry(SnapshotMode.KEY).setNumber(snapshotMode.ordinal());
+    public CameraMode getCameraMode() {
+        return cameraMode;
+    }
+
+    public void setCameraMode(final CameraMode cameraMode) {
+        this.cameraMode = cameraMode;
+    }
+
+    public Pipeline getPipeline() {
+        return pipeline;
+    }
+
+    public void setPipeline(final Pipeline pipeline) {
+        this.pipeline = pipeline;
+    }
+
+    public StreamMode getStreamMode() {
+        return streamMode;
+    }
+
+    public void setStreamMode(final StreamMode streamMode) {
+        this.streamMode = streamMode;
+    }
+
+    public SnapshotMode getSnapshotMode() {
+        return snapshotMode;
+    }
+
+    public void setSnapshotMode(final SnapshotMode snapshotMode) {
+        this.snapshotMode = snapshotMode;
     }
 }
