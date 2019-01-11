8ffaa52fb42685eb39c49616ed4534e439760ca4
Cole French <16979554+ColeFrench@users.noreply.github.com>
2019-01-11 17:03:52 -0500

    Add CameraSettings methods to Limelight

--- src/main/java/com/_2train395/limelight/api/Limelight.java
+++ src/main/java/com/_2train395/limelight/api/Limelight.java
@@ -15,6 +15,38 @@ public class Limelight {
         return NetworkTableInstance.getDefault().getTable(KEY);
     }
 
+    private static void setLedMode(final LedMode ledMode) {
+        getTable().getEntry(LedMode.KEY).setNumber(ledMode.ordinal());
+    }
+
+    private static void setCameraMode(final CameraMode cameraMode) {
+        getTable().getEntry(CameraMode.KEY).setNumber(cameraMode.ordinal());
+    }
+
+    private static void setPipeline(final Pipeline pipeline) {
+        getTable().getEntry(Pipeline.KEY).setNumber(pipeline.ordinal());
+    }
+
+    private static void setStreamMode(final StreamMode streamMode) {
+        getTable().getEntry(StreamMode.KEY).setNumber(streamMode.ordinal());
+    }
+
+    private static void setSnapshotMode(final SnapshotMode snapshotMode) {
+        getTable().getEntry(SnapshotMode.KEY).setNumber(snapshotMode.ordinal());
+    }
+
+    public static CameraSettings getCameraSettings() {
+        return new CameraSettings();
+    }
+
+    public static void setCameraSettings(final CameraSettings cameraSettings) {
+        setLedMode(cameraSettings.getLedMode());
+        setCameraMode(cameraSettings.getCameraMode());
+        setPipeline(cameraSettings.getPipeline());
+        setStreamMode(cameraSettings.getStreamMode());
+        setSnapshotMode(cameraSettings.getSnapshotMode());
+    }
+
     public static boolean hasTarget() {
         return getTable().getEntry(HAS_TARGETS_KEY).getDouble(0.0) == 1.0;
     }
