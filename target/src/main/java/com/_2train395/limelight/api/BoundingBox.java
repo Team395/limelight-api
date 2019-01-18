38b463247c95b38398ed542032c032bb9d7f48a7
Cole French <16979554+ColeFrench@users.noreply.github.com>
2019-01-18 07:16:25 -0500

    Document the BoundingBox class

--- target/src/main/java/com/_2train395/limelight/api/BoundingBox.java
+++ target/src/main/java/com/_2train395/limelight/api/BoundingBox.java
@@ -1,5 +1,8 @@
 package com._2train395.limelight.api;
 
+/**
+ * Represents the bounding box that Limelight fits around a target.
+ */
 public class BoundingBox {
     private static final String SHORTEST_SIDE_LENGTH_KEY = "tshort";
     private static final String LONGEST_SIDE_LENGTH_KEY = "tlong";
@@ -34,18 +37,30 @@ public class BoundingBox {
         return Limelight.getTable().getEntry(VERTICAL_SIDE_LENGTH_KEY).getDouble(0.0);
     }
 
+    /**
+     * @return the length of the shortest side of this fitted bounding box, in pixels
+     */
     public double getShortestSideLength() {
         return shortestSideLength;
     }
 
+    /**
+     * @return the length of the longest side of this fitted bounding box, in pixels
+     */
     public double getLongestSideLength() {
         return longestSideLength;
     }
 
+    /**
+     * @return the length of the horizontal side of the rough bounding box, ranging from 0 to 320 pixels
+     */
     public double getHorizontalSideLength() {
         return horizontalSideLength;
     }
 
+    /**
+     * @return the length of the vertical side of the rough bounding box, ranging from 0 to 320 pixels
+     */
     public double getVerticalSideLength() {
         return verticalSideLength;
     }
